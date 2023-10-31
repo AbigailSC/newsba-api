73f817f7e365a21255e0fbea5e06deb16c575084
Abigail Sarzuri <abigailsarzuri@gmail.com>
2023-10-31 15:05:48 -0300

    feat: add delete route favorite

--- src/routes/favorite.routes.ts
+++ src/routes/favorite.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { getFavoritesByUser, addFavorite } from '@controllers';
+import { getFavoritesByUser, addFavorite, deleteFavorite } from '@controllers';
 
 const router: Router = Router();
 
 router.route('/').get(getFavoritesByUser).post(addFavorite);
+
+router.route('/:id').delete(deleteFavorite);
 
 export default router;
